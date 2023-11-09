a79218a9cc7461410cd6c81d5f57d964c3366bb7
Sergio Ródenas <sergiorodenasgomez@gmail.com>
2023-11-09 13:40:15 +0000

    Fix

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -3,6 +3,7 @@ import { extend } from 'flarum/common/extend';
 import PostStream from 'flarum/forum/components/PostStream';
 import MoreDiscussions from './components/MoreDiscussions';
 import insertAfter from './helpers/insertAfter';
+import { ComponentAttrs } from 'flarum/common/Component';
 
 app.initializers.add('datlechin/flarum-more-discussions', () => {
   extend(PostStream.prototype, 'oncreate', function () {
@@ -11,6 +12,6 @@ app.initializers.add('datlechin/flarum-more-discussions', () => {
 
     const moreDiscussionRoot = document.createElement('div');
     moreDiscussionRoot.classList.add('MoreDiscussions');
-    m.mount(insertAfter(moreDiscussionRoot, postStream), MoreDiscussions);
+    m.mount(insertAfter(moreDiscussionRoot, postStream), MoreDiscussions<ComponentAttrs>);
   });
 });
